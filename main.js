38913b1174a716b7564fa3d118f1787593fb3997
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-09-08 16:59:05 +0300

    fix eslint Problems

---
+++ main.js
@@ -0,0 +1,57 @@
+// CONST & LET
+
+/*
+// let name = 'John';
+// name = 'Jack';
+
+// const person = {
+//   name: 'John',
+//   age: 33,
+// };
+
+// person.name = 'Jack';
+
+// const nums = [1, 2, 3, 4];
+// nums.push(5);
+// console.log(nums);
+*/
+
+// ARROW FUNCTIONS
+
+/*
+// function sayHello() {
+//   console.log('Hello');
+// }
+
+// const sayHello = name => console.log(`Hello ${name}`);
+*/
+
+// FOR EACH
+
+/*
+// const fruits = ['Apples', 'Bananas', 'Oranges', 'Grapes'];
+// fruits.forEach((fruit, index) => console.log(fruit));
+*/
+
+// MAP
+/*
+// const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+*/
+// FILTER
+
+const people = [
+  {
+    id: 1,
+    name: 'Karen'
+  },
+  {
+    id: 2,
+    name: 'Bob'
+  },
+  {
+    id: 3,
+    name: 'Sharon'
+  }
+];
+const people2 = people.filter(person => person.id !== 2);
+console.log(people2);
