925bfb91d27981ad74f50fb7d88ae1e80e6fa11b
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-09-10 09:06:25 +0300

    ES6 Refresher Updated

--- main.js
+++ main.js
@@ -73,3 +73,48 @@
 // // console.log(name, adress.street, hobbies[0]);
 // console.log(street, city);
  */
+
+// CLASSES
+/* 
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     return `Hello, my name is ${this.name} and i'm ${this.age}`;
+//   }
+// }
+
+// const person1 = new Person('John', 33);
+// const person2 = new Person('Sarah', 6);
+ */
+// SUBCLASSES
+/* 
+// class Customer extends Person {
+//   constructor(name, age, balance) {
+//     super(name, age);
+//     this.balance = balance;
+//   }
+
+//   info() {
+//     return `${this.name} owes $${this.balance}.00`;
+//   }
+// }
+
+// const customer1 = new Customer('Kevin', 32, 300);
+// console.log(customer1.info());
+ */
+// MODULES
+
+/* 
+// // file 1 (file1.js)
+// export const name = 'Jeff';
+// export const nums = [1, 2, 3];
+// export default Person;
+
+// // // file 2 (file2.js)
+// import { name, nums } from './file1';
+// import Person from './file1'; 
+*/
